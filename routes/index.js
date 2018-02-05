fd9d7ba85e30787a7cc99fc59756ef0f3b37583f
Josue <josuer22g@gmail.com>
2018-02-04 21:38:45 -0700

    Antes del video 17

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const productCtrl = require('../controllers/product');
 const api = express.Router();
-const auth = require('middlewares/auth');
+const auth = require('../middlewares/auth');
 
 
 
@@ -17,8 +17,8 @@ api.put('/product/:productId', productCtrl.updateProduct);
 
 api.delete('/product/:productId', productCtrl.deleteProduct);
 
-api.get('private', auth.isAuth, function(req, res){
-    res.status(200).send({message: `Tienes acceso`})
+api.get('private', auth, (req, res) =>{
+    res.status(200).send({message: `Tienes acceso`});
 });
 
 module.exports = api;
--- services/index.js
+++ services/index.js
@@ -14,4 +14,31 @@ function createToken(user){
    return jwt.encode(payload, config.SECRET_TOKEN );
 }
 
-module.exports = createToken;
+function decodeToken(token){
+    const decode = new Promise((resolve, reject)=>{
+        try{
+            const payload = jwt.decode(token, config.SECRET_TOKEN);
+
+            if(payload.exp <= moment().unix()){
+                reject({
+                    status: 401,
+                    message: 'El token ha expirado'
+                });
+            }
+
+            resolve(payload.sub);
+
+        }catch(err){
+            reject({
+                status: 500,
+                message: 'Invalid Token'
+            });
+        }
+    });
+    return decode;
+}
+
+module.exports = {
+    createToken,
+    decodeToken
+};
